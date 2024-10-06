3a9081dd430e2cfbbfb60fafe693d7f34a1acd8b
tanjir43 <tanjirislam7@gmail.com>
2024-10-06 15:22:03 +0600

    update portfolio features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,19 +158,14 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  Hackathons
+                  Additional Development Projects
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                   I like building things
                 </h2>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals.
-                </p>
+                {/* <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I secured 7th place in the Cybersecurity Contest at City University, demonstrating my proficiency and commitment in tackling complex challenges in digital security.
+                </p> */}
               </div>
             </div>
           </BlurFade>
